20e47dd7795740d144c37add334732d843d35a02
Roger Hom <roger.c.hom@gmail.com>
2016-01-05 00:53:38 -0800

    Start project.

---
+++ SO Tracker/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  SO Tracker
+//
+//  Created by psytronx on 1/3/16.
+//  Copyright © 2016 Logical Dimension. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+
+@end
+
---
+++ SO Tracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SO Tracker
+//
+//  Created by psytronx on 1/3/16.
+//  Copyright © 2016 Logical Dimension. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
